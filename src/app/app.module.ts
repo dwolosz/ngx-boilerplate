17b4d30ac06aa0a1ea633bf24da036743ad4afa2
Dawid Wołosz <dawid.wolosz@dawo.pl>
2018-02-21 13:23:54 +0100

    updateting to latest angular version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {DatePipe} from '@angular/common';
 import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
+import {StoreModule} from '@ngrx/store';
+
 import {AppRoutingModule} from './app-routing.module';
 import {SettingsService} from './services/settings.service';
 import {HttpService} from './services/http.service';
-import {AppComponent} from './app.component';
 import {DataResolveService} from './services/data-resolve.service';
-import {DatePipe} from '@angular/common';
+import {AppComponent} from './app.component';
+import {appReducer} from './app.reducer';
+
+
+
 
 @NgModule({
   declarations: [
@@ -19,6 +25,7 @@ import {DatePipe} from '@angular/common';
     BrowserAnimationsModule,
     HttpModule,
     AppRoutingModule,
+    StoreModule.forRoot({ui: appReducer}),
     ScrollToModule.forRoot()
   ],
   providers: [SettingsService, HttpService, DataResolveService, DatePipe],
--- src/app/modules/core/core.component.ts
+++ src/app/modules/core/core.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import * as fromApp from '../../app.reducer';
+import {Store} from '@ngrx/store';
 
 @Component({
   selector: 'app-page',
@@ -6,8 +8,18 @@ import {Component, OnDestroy} from '@angular/core';
 })
 export class CoreComponent implements OnDestroy {
 
+  private sTimeout: Number;
 
-  constructor() {
+
+  constructor(private store: Store<{ui: fromApp.State }>) {
+    this.sTimeout = window.setTimeout(() => {
+      // this.store.dispatch({type: 'STOP_LOADING'});
+    }, 250);
+  }
+
+  ngOnInit(){
+    // this.store.dispatch({type: 'START_LOADING'});
+
   }
 
   ngOnDestroy() {
